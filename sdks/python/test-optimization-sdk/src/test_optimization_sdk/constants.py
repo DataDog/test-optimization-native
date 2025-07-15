7c24f7e91036aa6c1f7d5e85c56534860f3aef79
Tony Redondo <tony.redondo@datadoghq.com>
2025-07-15 17:47:35 +0200

    bump version

--- sdks/python/test-optimization-sdk/src/test_optimization_sdk/constants.py
+++ sdks/python/test-optimization-sdk/src/test_optimization_sdk/constants.py
@@ -5,4 +5,4 @@ Constants used throughout the test optimization SDK.
 # Environment variable constants
 TEST_OPTIMIZATION_SDK_SKIP_NATIVE_INSTALL = "TEST_OPTIMIZATION_SDK_SKIP_NATIVE_INSTALL"
 TEST_OPTIMIZATION_SDK_NATIVE_SEARCH_PATH = "TEST_OPTIMIZATION_SDK_NATIVE_SEARCH_PATH" 
-TEST_OPTIMIZATION_DOWNLOAD_URL_FORMAT = "https://github.com/DataDog/test-optimization-native/releases/download/v0.0.2-preview/"
+TEST_OPTIMIZATION_DOWNLOAD_URL_FORMAT = "https://github.com/DataDog/test-optimization-native/releases/download/v0.0.3-preview/"
