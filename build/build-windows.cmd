b968ff62eb89c25cdfa7b7bb4905fd101b300999
Tony Redondo <tony.redondo@datadoghq.com>
2025-07-15 17:34:51 +0200

    (fix): try to fix the windows build (#22)
    * (fix): try to fix the windows build
    
    * (fix): try to fix the windows build
    
    * fix toolchain
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * change
    
    * changes
    
    * changes
    
    * changes
    
    * revert unescessary changes

--- build/build-windows.cmd
+++ build/build-windows.cmd
@@ -5,13 +5,28 @@ set GOARCH=amd64
 set CC=x:\mingw64\bin\x86_64-w64-mingw32-gcc.exe
 set CGO_ENABLED=1
 set CGO_CPPFLAGS=
-set CGO_CXXFLAGS=-O2 -g
-set CGO_FFLAGS=-O2 -g
-set CGO_CFLAGS=-O2 -Os -DNDEBUG
-set CGO_LDFLAGS=-Wl,--gc-sections
+set CGO_CXXFLAGS=-O2
+set CGO_FFLAGS=-O2
+set CGO_CFLAGS=-O2 -fno-unwind-tables -fno-asynchronous-unwind-tables
+set CGO_LDFLAGS=-Wl,--no-seh
 
 echo Building windows static library
-go build -tags civisibility_native -buildmode=c-archive -ldflags="-s -w" -o ./output/windows-x64-libtestoptimization-static/testoptimization.lib exports.go main.go
+go build -tags civisibility_native -buildmode=c-archive -ldflags "-linkmode=external -extldflags=-Wl,--no-seh" -o ./output/windows-x64-libtestoptimization-static/testoptimization.lib exports.go main.go
+
+echo Stripping .pdata and .xdata sections from the static library
+pushd ./output/windows-x64-libtestoptimization-static
+set "AR=x:\mingw64\bin\llvm-ar.exe"
+set "OBJCOPY=x:\mingw64\bin\llvm-objcopy.exe"
+
+%AR% x testoptimization.lib
+for %%f in (*.o) do (
+    echo     stripping %%f
+    %OBJCOPY% --remove-section=.pdata --remove-section=.xdata %%f
+)
+del testoptimization.lib
+%AR% rc testoptimization.lib *.o
+%AR% s  testoptimization.lib
+popd
 
 echo Building windows shared library
 go build -tags civisibility_native -buildmode=c-shared -ldflags="-s -w" -o ./output/windows-x64-libtestoptimization-dynamic/testoptimization.dll exports.go main.go
