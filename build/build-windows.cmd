be70455ed8c6630a6a9943d46b3b4a7127a4e743
Tony Redondo <tony.redondo@datadoghq.com>
2025-07-15 17:17:54 +0200

    revert unescessary changes

--- build/build-windows.cmd
+++ build/build-windows.cmd
@@ -5,17 +5,12 @@ set GOARCH=amd64
 set CC=x:\mingw64\bin\x86_64-w64-mingw32-gcc.exe
 set CGO_ENABLED=1
 set CGO_CPPFLAGS=
-rem set CGO_CXXFLAGS=-O2 -g
 set CGO_CXXFLAGS=-O2
-rem set CGO_FFLAGS=-O2 -g
 set CGO_FFLAGS=-O2
-rem set CGO_CFLAGS=-O2 -Os -DNDEBUG
 set CGO_CFLAGS=-O2 -fno-unwind-tables -fno-asynchronous-unwind-tables
-rem set CGO_LDFLAGS=-Wl,--gc-sections
 set CGO_LDFLAGS=-Wl,--no-seh
 
 echo Building windows static library
-rem go build -tags civisibility_native -buildmode=c-archive -ldflags="-s -w" -o ./output/windows-x64-libtestoptimization-static/testoptimization.lib exports.go main.go
 go build -tags civisibility_native -buildmode=c-archive -ldflags "-linkmode=external -extldflags=-Wl,--no-seh" -o ./output/windows-x64-libtestoptimization-static/testoptimization.lib exports.go main.go
 
 echo Stripping .pdata and .xdata sections from the static library
